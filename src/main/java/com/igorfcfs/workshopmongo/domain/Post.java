29c393f39bfc6cf762c9037e6c1db6dd60198334
Jorge Silva <igorfcfs@gmail.com>
2021-05-21 20:53:40 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/igorfcfs/workshopmongo/domain/Post.java
+++ src/main/java/com/igorfcfs/workshopmongo/domain/Post.java
@@ -1,23 +1,27 @@
 package com.igorfcfs.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.igorfcfs.workshopmongo.dto.AuthorDTO;
+import com.igorfcfs.workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
 	private static final long serialVersionUID = 1L;
-
 	@Id
 	private String id;
 	private Date date;
 	private String title;
 	private String body;
 	private AuthorDTO author;
+
+	private List<CommentDTO> comments = new ArrayList<>();
 
 	public Post() {
 	}
@@ -30,45 +34,43 @@ public class Post implements Serializable {
 		this.body = body;
 		this.author = author;
 	}
-
 	public String getId() {
 		return id;
 	}
-
 	public void setId(String id) {
 		this.id = id;
 	}
-
 	public Date getDate() {
 		return date;
 	}
-
 	public void setDate(Date date) {
 		this.date = date;
 	}
-
 	public String getTitle() {
 		return title;
 	}
-
 	public void setTitle(String title) {
 		this.title = title;
 	}
-
 	public String getBody() {
 		return body;
 	}
-
 	public void setBody(String body) {
 		this.body = body;
 	}
-
 	public AuthorDTO getAuthor() {
 		return author;
 	}
-
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
+	}
+
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
 	}
 
 	@Override
@@ -78,7 +80,6 @@ public class Post implements Serializable {
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
-
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)

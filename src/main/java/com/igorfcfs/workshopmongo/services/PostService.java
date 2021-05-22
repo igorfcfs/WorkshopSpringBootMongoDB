958c47946d88efe76b6a606e69c91061f65ecdc9
Jorge Silva <igorfcfs@gmail.com>
2021-05-21 21:05:29 -0300

    Consulta simples com query methods

--- src/main/java/com/igorfcfs/workshopmongo/services/PostService.java
+++ src/main/java/com/igorfcfs/workshopmongo/services/PostService.java
@@ -9,19 +9,17 @@ import org.springframework.stereotype.Service;
 import com.igorfcfs.workshopmongo.domain.Post;
 import com.igorfcfs.workshopmongo.repositories.PostRepository;
 import com.igorfcfs.workshopmongo.services.exceptions.ObjectNotFoundException;
-
 @Service
 public class PostService {
-	
 	@Autowired
 	private PostRepository repo;
-	
-	public List<Post> findAll(){
-		return repo.findAll();
-	}
 	
 	public Post findById(String id) {
 		Optional<Post> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+	public List<Post> findByTitle(String text) {
+		return repo.findByTitleContainingIgnoreCase(text);
 	}
 }
